d6d3691f61d4584ed51e83410c579ba4f2ecbf40
Jduitama <johandav2@gmail.com>
2026-07-30 19:54:59 -0500

    Revert "fix: load Vue dashboard on static hosting"
    This reverts commit 29f16de461188fa6d087138030a762443c6f5dbc.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from "https://cdn.jsdelivr.net/npm/vue@3.5.13/dist/vue.esm-browser.prod.js";
+import { createApp } from "vue";
 import App from "./App.js";
 import "./styles.css";
 
