62f968b53f543225540934e0a3a9d177245d7d5d
Jduitama <johandav2@gmail.com>
2026-07-30 19:54:59 -0500

    Revert "feat: migrate dashboard navigation to Vue"
    This reverts commit d618ef64de0260349f27d369cd893ff3ada5c069.

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import Navbar from "./components/Navbar.js";
-
-export default {
-  components: { Navbar },
-  template: `<Navbar /><main class="dashboard-content" id="inicio"><section class="dashboard-card"><h1>Bienvenido a tu panel</h1><p>Gestiona la información de tu organización desde el menú lateral.</p></section></main>`
-};
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.js";
-import "./styles.css";
-
-createApp(App).mount("#app");
